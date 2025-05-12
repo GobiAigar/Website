28e15addddc9de0707256239862fc9916b55f2dc
ligthman <jamka888@gmail.com>
2025-05-12 12:03:23 +0800

    second

---
+++ web/src/app/(pages)/layout.tsx
@@ -0,0 +1,16 @@
+import Footer from "@/components/statics/Footer";
+import { Navbar } from "@/components/statics/Navbar";
+
+export default function Layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div>
+      <Navbar />
+      {children}
+      <Footer />
+    </div>
+  );
+}
