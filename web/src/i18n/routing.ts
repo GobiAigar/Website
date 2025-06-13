d58ee4e429008c05175a0c21b312f7dd23048cf3
unknown <Jamka888@gmail.com>
2025-06-13 14:31:01 +0800

    default is mongolia

--- web/src/i18n/routing.ts
+++ web/src/i18n/routing.ts
@@ -3,6 +3,6 @@ import { defineRouting } from "next-intl/routing";
 export const routing = defineRouting({
   locales: ["en", "mn"],
 
-  defaultLocale: "en",
+  defaultLocale: "mn",
   localePrefix: "always",
 });
